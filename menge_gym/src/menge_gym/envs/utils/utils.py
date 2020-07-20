dd46795ee603a2f8e3749744bbbf5d043a71ca2d
Julian Kunze <julian.kunze@tum.de>
2020-07-20 22:48:33 +0200

    Adapt gym env reward estimation to interpolate between discrete position
    Interpolate between last and current robot position to check for collisions

--- menge_gym/src/menge_gym/envs/utils/utils.py
+++ menge_gym/src/menge_gym/envs/utils/utils.py
@@ -45,3 +45,62 @@ class DeviationWindow(object):
         self.index = 0
         self.content = self.size * [0]
         self.deviations = self.size * [0]
+
+
+def point_to_segment_dist(a: np.ndarray, b: np.ndarray, x: np.ndarray):
+    """
+    Calculate the shortest distance between point x and all line segments with two endpoints a[i], b[i]
+
+    @:param: a: start point(s) of the line segment(s) (shape ... x d)
+    @:param: b: end point(s) of the line segment(s)   (shape ... x d)
+    @:param: x: point(s) to compute distance to       (shape ... x d)
+
+    :return: d_min: shortest distance(s) for all lines/points
+    """
+    if len(a.shape) == 1:
+        a = a.reshape(1, -1)
+    if len(b.shape) == 1:
+        b = b.reshape(1, -1)
+    if len(x.shape) == 1:
+        x = x.reshape(1, -1)
+
+
+    assert a.shape == b.shape, "Number of start and end points must be equal"
+    assert a.shape[-1] == x.shape[-1], "Line and Point need to have same number of coordinates " \
+                                       "(line: {}, point: {}".format(a.shape[1], x.shape[1])
+    num_segments = len(a)
+    num_points = len(x)
+
+    if num_points != 1 and num_segments != 1 and num_points != num_segments:
+        # No 1-to-1, 1-to-many, n-to-n but n-to-m comparison --> requires dim expansion
+        a = np.repeat(a[np.newaxis, ...], num_points, axis=0)
+        b = np.repeat(b[np.newaxis, ...], num_points, axis=0)
+        x = np.repeat(x[:, np.newaxis, :], num_segments, axis=1)
+
+    ab = b - a
+    ax = x - a
+    bx = x - b
+    # compute points on line through a and b that are closest to x
+    # equivalent formula for single point and line: a + dot(ax, ab)/dot(ab, ab) * ab
+    # stabilized by adding EPS in denominator
+    closest_points = a + ((ax * ab).sum(-1) / (ab * ab + EPS).sum(-1))[..., np.newaxis] * ab
+
+    # determine whether closest_points lie on line segment
+    ac = closest_points - a
+    dot_product = (ab * ac).sum(-1)
+    squared_norm_ab = (ab * ab).sum(-1)
+    mask = np.bitwise_and(dot_product >= 0, dot_product <= squared_norm_ab)
+
+    # distance a - x
+    dist_a = np.linalg.norm(ax, axis=-1)
+    # distance b - x
+    dist_b = np.linalg.norm(bx, axis=-1)
+    # distance closest point on line - x
+    dist_c = np.linalg.norm(closest_points - x, axis=-1)
+
+    # if closest point on line outside line segment -> shortest distance to one of the segments bounds
+    d_min = np.minimum(dist_a, dist_b)
+    # if closest point on line inside line segment -> shortest distance to closest point
+    d_min[mask] = dist_c[mask]
+
+    return d_min
